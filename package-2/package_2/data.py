2d54355c10d5d3dc2ab4c921430a05955e665dfc
Claudius Kienle <privat@claudiuskienle.de>
2022-12-07 19:34:53 +0100

    minimal example

---
+++ package-2/package_2/data.py
@@ -0,0 +1,3 @@
+class DataClass:
+    def __init__(self):
+        pass
---
+++ package-2/package_2/main.py
@@ -0,0 +1,6 @@
+from package_2.data import DataClass # this works as intended
+# from package_1.data_utils import transform_data # VSCode 'code actions' does not list this import for transform_data
+
+
+data = DataClass()
+data = transform_data()
